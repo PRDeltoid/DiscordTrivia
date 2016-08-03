eaf3f80b74e170d64e587c5b5155f4bedbb6abd0
PRDeltoid <tbrit10@gmail.com>
2016-08-02 18:17:46 -0700

    Added question_count variable to Database
    Added a count variable to Database so that a random question can be
    chosen within the range of questions available
    
    Also did misc. cleanup of trailing spaces and some other things

--- lib/database.rb
+++ lib/database.rb
@@ -1,15 +1,16 @@
 require 'mysql2'
 
-class Database 
+class Database
   attr_accessor :client
-  attr_reader :question_table
+  attr_reader :question_table, :question_count
 
   def initialize()
-    @client = Mysql2::Client.new(:host => 'localhost', 
+    @client = Mysql2::Client.new(:host => 'localhost',
                                  :username => $config.get('username'),
-                                 :password => $config.get('password'), 
+                                 :password => $config.get('password'),
                                  :database => $config.get('database'))
     @question_table = $config.get('question_table')
+    @question_count = client.query("SELECT COUNT(*) AS question_count FROM #{question_table}").first["question_count"]
   end
 
   #hardcoded row
